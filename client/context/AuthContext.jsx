01f8bc14775fbf5482d544f145642b6914e4d1b6
Harshit Aswal <123983494+harshitaswal@users.noreply.github.com>
2026-07-17 22:39:08 +0530

    Update backend URL in AuthContext

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import toast from "react-hot-toast";
 import { io } from "socket.io-client"
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL;
+const backendUrl = "https://quick-chat-backend-w2op.onrender.com"
 axios.defaults.baseURL = backendUrl;     
 
 export const AuthContext = createContext();
